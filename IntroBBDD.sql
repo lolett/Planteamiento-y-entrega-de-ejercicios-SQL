70b8131a61c4ae36c128739cbb1be92733aa186b
Manu <manuel.aparicio.caballero@gmail.com>
2024-09-20 23:56:40 +0200

    correction mistakes

--- IntroBBDD.sql
+++ IntroBBDD.sql
@@ -1,5 +1,5 @@
 -- 1. Escribe una consulta que recupere los Vuelos (flights) y su identificador que figuren con status On Time.
-SELECT flight_id
+SELECT flight_id, fligh_no, status
 FROM flights
 WHERE status = 'On Time';
 
